0b852b73d3a82235e6ddc24d49fee1684c463ae2
Leonardo Rota-Rossi <leorossi@gmail.com>
2022-06-29 13:31:16 -0400

    feat(semantic-release): adds semantic-release support

---
+++ utils/versioning_ios.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# exit if a command fails
+set -e
+
+PLIST_BUDDY_COMMAND="/usr/libexec/PlistBuddy"
+
+if [ -f ${PLIST_BUDDY_COMMAND} ];
+then
+  APP_NAME=`jq .name  app.json | sed 's/"//g'`
+  NEW_VERSION_NAME=$1
+  # to be generated using GH Action
+  NEW_VERSION_CODE="${GITHUB_RUN_ID}${GITHUB_RUN_ATTEMPT}"
+
+  # Generates a version code that's versionName without dots.
+  if [ -z "${NEW_VERSION_CODE}" ] 
+  then 
+    NEW_VERSION_CODE=`echo ${NEW_VERSION_NAME} | sed 's/\.//g'`
+  fi
+
+  INFO_PLIST_FILE="./ios/${APP_NAME}/Info.plist"
+
+  CURRENT_VERSION_CODE=`${PLIST_BUDDY_COMMAND} -c "Print CFBundleVersion" "${INFO_PLIST_FILE}"`
+  CURRENT_VERSION_NAME=`${PLIST_BUDDY_COMMAND} -c "Print CFBundleShortVersionString" "${INFO_PLIST_FILE}"`
+
+  echo "Old Version: ${CURRENT_VERSION_NAME}"
+  echo "Old Version Code: ${CURRENT_VERSION_CODE}"
+  echo "New Version: ${NEW_VERSION_NAME}"
+  echo "New Version Code: ${NEW_VERSION_CODE}"
+
+  $PLIST_BUDDY_COMMAND -c "Set :CFBundleVersion ${NEW_VERSION_CODE}" "${INFO_PLIST_FILE}"
+  $PLIST_BUDDY_COMMAND -c "Set :CFBundleShortVersionString ${NEW_VERSION_NAME}" "${INFO_PLIST_FILE}"
+
+else 
+  echo "PlistBuddy not found - possibly not macOS"
+fi
