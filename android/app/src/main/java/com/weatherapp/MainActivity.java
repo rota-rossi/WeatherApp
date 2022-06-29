329c011591448341c911a519a4b4b69650da09bf
Leonardo Rota-Rossi <leorossi@gmail.com>
2022-06-29 15:56:50 -0400

    feat(splash): updates icon, splash and bundle name

--- android/app/src/main/java/com/weatherapp/MainActivity.java
+++ android/app/src/main/java/com/weatherapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.weatherapp;
+package net.weblers.weatherapp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/weatherapp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/weatherapp/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.weatherapp.newarchitecture.modules;
+package net.weblers.weatherapp.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
